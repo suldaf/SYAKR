9500bf43526cf64a3bafe42bc7234274a1156674
Alif Taufik <alif.tr.aminuddin@gmail.com>
2021-04-16 14:01:58 +0700

    Initialize Project Configs and defaults - Installing tailwindcss framework - Updating config files - Remove unused components and files - Update router

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,36 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint",
   },
+  settings: {
+    "import/resolver": {
+      "babel-module": {
+        plugins: [
+          [
+            "module-resolver",
+            {
+              root: ["./src"],
+              alias: {
+                "@": "./src",
+              },
+            },
+          ],
+        ],
+      },
+    },
+  },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        tabWidth: 2,
+        semi: true,
+        trailingComma: "es5",
+        arrowParens: "always",
+      },
+    ],
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
   },
 };
